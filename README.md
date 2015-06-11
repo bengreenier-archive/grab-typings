1a6a9fec684524c7ad841d649e1855e7c9a7a3f9
b3ngr33ni3r <ben@bengreenier.com>
2015-06-10 22:48:50 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# grab-typings
+
+grab definitelyTyped typings for each module in package.json.
+
+## Getting Started
+
+just `npm install -g grab-typings` and then `grab-typings` from a project directory (anywhere with a `package.json`).
+
+## Example output
+
+```bash
+404 | request-promise/request-promise.d.ts
+404 | promise/promise.d.ts
+200 | a:\vs_workspace\grab-typings\typings\node\node.d.ts
+```
+
+The layout is really simple; `<http status code>` | `<path>`.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+/// <reference path="typings/node/node.d.ts"/>
+
+var path = require('path');
+var fs = require('fs');
+
+var rp = require('request-promise');
+var Promise = require('promise');
+
+var pkg = require('./package.json');
+var dir = path.normalize(__dirname + "/" + "typings");
+
+var deps = ["node"];
+for (var prop in pkg["dependencies"]) {
+	deps.push(prop);
+}
+for (var prop in pkg["devDependencies"]) {
+	deps.push(prop);
+}
+
+function proc(pth, body) {
+	pth = path.normalize(dir + "/" + pth);
+	try{
+		fs.mkdirSync(dir);
+	}catch(e){}
+	try{
+		var pthdir = path.dirname(pth);
+		fs.mkdirSync(pthdir);
+	}catch(e){}
+	fs.writeFileSync(pth, prom.body);
+	console.log("200 | " + pth);
+}
+
+function bproc(path, res) {
+	console.error(res.statusCode + " | " + path);
+}
+
+for (var i = 0 ; i < deps.length ; i++) {
+	var prom = rp("https://github.com/borisyankov/DefinitelyTyped/raw/master/"+deps[i]+"/"+deps[i]+".d.ts");
+	prom.then(proc.bind(this, deps[i]+"/"+deps[i]+".d.ts"), bproc.bind(this, deps[i]+"/"+deps[i]+".d.ts"));
+}
