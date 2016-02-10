8a966cd40ed14faa5a3736616b63340d3a91b4c9
benfahrenheit <benfahrenheit@users.noreply.github.com>
2016-02-09 22:02:36 -0500

    Why not add a little color to your life?

--- lib/grab-typings.ts
+++ lib/grab-typings.ts
@@ -2,6 +2,7 @@
 /// <reference path="../typings/node/node.d.ts" />
 
 import * as GT from './';
+import * as chalk from 'chalk';
 
 /**
  * A simple little command line version
@@ -13,14 +14,14 @@ new GT.GrabTypings().run(process.argv.slice(2)).then((rr : GT.RunResult) => {
     
     rr.installed.forEach((m : string) => {
         // checkmark <module>
-        console.log("\u2713 "+m);
+        console.log(chalk.green("\u2713 ")+m);
     });
     rr.warnings.forEach((message : string) => {
         console.warn(message);
     });
     rr.missing.forEach((m : string) => {
         // x <module>
-        console.error("\u2718 "+m);
+        console.error(chalk.red("\u2718 ")+m);
     });
 }, (err : any) => {
     console.error("Oops - something went wrong...", err);
